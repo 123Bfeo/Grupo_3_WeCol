8833edebea83344daf7f78fff2f8e16092cc9d72
PRJennifer <jennifer.perezr21@gmail.com>
2022-03-09 19:17:18 -0500

    admin create changes

--- src/routers/appMain.js
+++ src/routers/appMain.js
@@ -1,20 +1,16 @@
-
 //Importaciones nativas
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const mainRoutes = express.Router();
 
-
-const CONTROLLER_PATH = path.resolve('src/controllers/mainController');
+const CONTROLLER_PATH = path.resolve("src/controllers/mainController");
 const mainController = require(CONTROLLER_PATH);
 
-mainRoutes.get('/', mainController.index);
-mainRoutes.get('/aboutUs', mainController.aboutUs);
-mainRoutes.get('/contact', mainController.contact);
-mainRoutes.get('/privacyPolitics', mainController.privacyPolitics);
-mainRoutes.get('/agreePolitics', mainController.agreePolitics);
+mainRoutes.get("/", mainController.index);
+mainRoutes.get("/aboutUs", mainController.aboutUs);
+mainRoutes.get("/contact", mainController.contact);
+mainRoutes.get("/privacyPolitics", mainController.privacyPolitics);
+mainRoutes.get("/agreePolitics", mainController.agreePolitics);
+mainRoutes.get("/adminCreate", mainController.adminCreate);
 
 module.exports = mainRoutes;
-
-
-
