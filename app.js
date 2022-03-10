9e00650923d8787c2bd3a88698320595246a5c62
PRJennifer <jennifer.perezr21@gmail.com>
2022-03-10 18:09:34 -0500

    cambios para Heroku

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
-const mainRoutes = require('./src/routers/main.routes');
-const productRoutes = require('./src/routers/products.routes');
-const factureController = require('./src/routers/facture.routes');
-const userController = require('./src/routers/users.routes');
+const path = require("path");
+const mainRoutes = require("./src/routers/main.routes");
+const productRoutes = require("./src/routers/products.routes");
+const factureController = require("./src/routers/facture.routes");
+const userController = require("./src/routers/users.routes");
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'src/views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "src/views"));
 
-const publicPath = path.resolve('public');
+const publicPath = path.resolve("public");
 app.use(express.static(publicPath));
 
 // Invocación de rutas
-app.use('/', mainRoutes, productRoutes, factureController, userController);
+app.use("/", mainRoutes, productRoutes, factureController, userController);
 
 const port = 3000;
-app.listen(port, () => {
-  console.log(`El servidor se está ejecutando en el puerto ${port}`);
+app.listen(process.env.PORT || port, () => {
+  console.log(`Servidor corriendo en el puerto ${port}`);
 });
--- src/routers/main.routes.js
+++ src/routers/main.routes.js
@@ -3,8 +3,7 @@ const express = require("express");
 const path = require("path");
 const mainRoutes = express.Router();
 
-<<<<<<< HEAD:src/routers/appMain.js
-const CONTROLLER_PATH = path.resolve("src/controllers/mainController");
+const CONTROLLER_PATH = path.resolve("src/controllers/main.controller");
 const mainController = require(CONTROLLER_PATH);
 
 mainRoutes.get("/", mainController.index);
@@ -12,18 +11,7 @@ mainRoutes.get("/aboutUs", mainController.aboutUs);
 mainRoutes.get("/contact", mainController.contact);
 mainRoutes.get("/privacyPolitics", mainController.privacyPolitics);
 mainRoutes.get("/agreePolitics", mainController.agreePolitics);
-mainRoutes.get("/adminCreate", mainController.adminCreate);
-=======
-const CONTROLLER_PATH = path.resolve('src/controllers/main.controller');
-const mainController = require(CONTROLLER_PATH);
-
-mainRoutes.get('/', mainController.index);
-mainRoutes.get('/aboutUs', mainController.aboutUs);
-mainRoutes.get('/contact', mainController.contact);
-mainRoutes.get('/privacyPolitics', mainController.privacyPolitics);
-mainRoutes.get('/agreePolitics', mainController.agreePolitics);
-mainRoutes.get('/register', mainController.register);
-mainRoutes.get('/login', mainController.login);
->>>>>>> main:src/routers/main.routes.js
+mainRoutes.get("/register", mainController.register);
+mainRoutes.get("/login", mainController.login);
 
 module.exports = mainRoutes;
