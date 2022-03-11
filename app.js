49c6aaa479bf9c4bd71f7acbf42d528f63988bac
Sergio González Sánchez <SoySergioGonzalez@Outlook.es>
2022-03-10 22:38:30 -0500

    Se actualiza el puerto para usar para Heroku

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const path = require("path");
-const mainRoutes = require("./src/routers/main.routes");
-const productRoutes = require("./src/routers/products.routes");
-const factureController = require("./src/routers/facture.routes");
-const userController = require("./src/routers/users.routes");
+const path = require('path');
+const mainRoutes = require('./src/routers/main.routes');
+const productRoutes = require('./src/routers/products.routes');
+const factureController = require('./src/routers/facture.routes');
+const userController = require('./src/routers/users.routes');
 
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "src/views"));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'src/views'));
 
-const publicPath = path.resolve("public");
+const publicPath = path.resolve('public');
 app.use(express.static(publicPath));
 
 // Invocación de rutas
-app.use("/", mainRoutes, productRoutes, factureController, userController);
+app.use('/', mainRoutes, productRoutes, factureController, userController);
 
-app.listen(process.env.PORT || 3030, () => {
-  console.log(`Servidor corriendo en el puerto 3030`);
+const port = 3030;
+app.listen(process.env.PORT || port, () => {
+  console.log(`Servidor corriendo en el puerto ${port}`);
 });
