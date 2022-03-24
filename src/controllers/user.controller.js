e3f0214fa6cd594ede7fac6ede60af6d2b628f68
Sergio González Sánchez <SoySergioGonzalez@Outlook.es>
2022-03-23 23:38:54 -0500

    Se realizan cambios a main

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,30 +1,33 @@
-
 const fs = require('fs');
 const path = require('path');
-const adminUsers = JSON.parse(fs.readFileSync(path.resolve('src/data/adminUsers.JSON')));
+const adminUsers = JSON.parse(
+  fs.readFileSync(path.resolve('src/data/adminUsers.JSON'))
+);
 
 const userController = {
   register: (req, res) => {
-    const title = 'Registro'
-    res.render('./users/register',{title});
+    const title = 'Registro';
+    res.render('./users/register', { title });
   },
   login: (req, res) => {
     let err = false;
-    const title = 'Login'
-    res.render('./users/login',{err:err,title});
+    const title = 'Login';
+    res.render('./users/login', { err: err, title });
   },
-  loginUser:(req,res)=>{ 
+  loginUser: (req, res) => {
     let email = req.body.email;
     let password = req.body.password;
-    let findUser = adminUsers.find(user => user.email==email&&user.password==password);
-    if(findUser){
+    let findUser = adminUsers.find(
+      (user) => user.email == email && user.password == password
+    );
+    if (findUser) {
       res.redirect('/adminCreate');
-    }else{
+    } else {
       let err = true;
-      const title = 'Login'
-      res.render('./users/login',{err:err,title});
-    } 
-  }
+      const title = 'Login';
+      res.render('./users/login', { err: err, title });
+    }
+  },
 };
 
 module.exports = userController;
