3a01c9ce9688c4651f302e8a020639ce860357e6
Sergio González Sánchez <soysergiogonzalez@outlook.es>
2022-04-25 15:36:18 -0500

    Se realizan cambios en productDet.ejs porque no funcionaba

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'src/views'));
 
 app.use(session({
-        secret: "Cruz",
+        secret: 'Cruz',
         resave: false,
         saveUninitialized: true
       }
