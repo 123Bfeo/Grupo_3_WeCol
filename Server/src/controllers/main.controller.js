704cdcb680348fbeee9a2a2159141eef87eeca9d
PRJennifer <Jennifer.perezr21@gmail.com>
2022-07-07 19:58:45 -0500

    se realizan cambios en la vista de Offers y en las validaciones del register

--- Server/src/controllers/main.controller.js
+++ Server/src/controllers/main.controller.js
@@ -4,42 +4,36 @@ const db = require('../../database/models');
 const mainController = {
   index: (req, res) => {
     const title = 'Home';
-    const reqCategory = db.Category.findAll();
-    const reqProduct = db.Product.findAll();
-
-    Promise.all([reqCategory, reqProduct])
-      .then(([category, product]) => {
-        res.render('index', { title, category, product })
-      });
+    // const reqCategory = db.Category.findAll();
+    // const reqProduct = db.Product.findAll();
+    // Promise.all([reqCategory, reqProduct])
+    //   .then(([category, product]) => {
+    res.render('index', { title })
+    // });
   },
-
-
-  aboutUs: (req, res) => {
+  offers: (req, res) => {
     const title = 'A cerca';
-    db.Category.findAll().then(function (category) {
-      res.render('aboutUs', { title, category });
-    })
+    res.render('offers', { title, });
   },
 
   contact: (req, res) => {
     const title = 'Contacto';
-    db.Category.findAll().then(function (category) {
-      res.render('contact', { title, category });
-    })
+    res.render('contact', { title });
+
   },
-  privacyPolitics: (req, res) => {
-    const title = 'Política de Privacidad';
-    res.render('privacyPolitics', { title });
-  },
-  agreePolitics: (req, res) => {
-    const title = 'Políticas';
-    res.render('agreePolitics', { title });
-  },
-  adminCreate: (req, res) => {
-    const title = 'Administrador Productos';
-    const products = productModel.read();
-    res.render('./admin/adminCreate', { products, title });
-  },
+  // privacyPolitics: (req, res) => {
+  //   const title = 'Política de Privacidad';
+  //   res.render('privacyPolitics', { title });
+  // },
+  // agreePolitics: (req, res) => {
+  //   const title = 'Políticas';
+  //   res.render('agreePolitics', { title });
+  // },
+  // adminCreate: (req, res) => {
+  //   const title = 'Administrador Productos';
+  //   const products = productModel.read();
+  //   res.render('./admin/adminCreate', { products, title });
+  // },
 };
 
 module.exports = mainController;
