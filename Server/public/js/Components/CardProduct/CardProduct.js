8dbfdda771d94e9320ff47b58636f929be0c8199
123Bfeo <ldnr13@hotmail.com>
2022-07-13 20:42:54 -0500

    se realizan cambios

--- Server/public/js/Components/CardProduct/CardProduct.js
+++ Server/public/js/Components/CardProduct/CardProduct.js
@@ -1,30 +1,32 @@
 const ProductsSectionHomeContent = document.getElementById('ProductsSectionHomeContent');
 
 fetch('http://localhost:3001/api/products')
-	.then((response) => response.json())
-	.then((data) => {
-		data.data.product.forEach((item) => {
-			const CardProduct = document.createElement('section');
-			CardProduct.innerHTML = `
+    .then((response) => response.json())
+    .then((data) => {
+        data.data.product.forEach((item) => {
+            const CardProduct = document.createElement('section');
+            CardProduct.innerHTML = `
         <article class="CardProduct">
 		    <picture class='CardProductHead'>
-		        <a href='/api/details/${item.id}' target="_blank">
-                    <img src="/img/products/${item.image}" alt="${ item.name }" title="${ item.name }">
+		        <a href='/api/details/${item.id}'>
+                    <img src="/img/products/${item.image}" alt="${item.name}" title="${item.name}">
                 </a>
             </picture>
             <section class="CardProductBody">
-                <span class="CardProductBodyNameProduct">${ item.name }</span>
+                <span class="CardProductBodyNameProduct">${item.name}</span>
                 <div class="CardProductBodyDescription">
                 <p>${item.description}</p>
                 </div>
                 <div class='CardProductBodyPrice'>
                 <span class="price">${item.price}</span>
+                <a href="/product/cart">
                 <button class="AddToCartButton">
                     <i class="fa-solid fa-cart-plus"></i>
                 </button>
+                </a>
                 </div>
             </section>
         </article>`;
-			ProductsSectionHomeContent.appendChild(CardProduct);
-		});
-	});
+            ProductsSectionHomeContent.appendChild(CardProduct);
+        });
+    });
